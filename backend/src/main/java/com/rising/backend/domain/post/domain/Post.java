15f1c6125df51e1fee79bd92e90e8b2921e67cc4
halin <halin130@naver.com>
2023-01-02 23:41:42 +0900

    chore: Backend 개발 환경 초기 세팅
    appiication.yml 내용 추가, 디렉토리 구조 뼈대 세팅

---
+++ backend/src/main/java/com/rising/backend/domain/post/domain/Post.java
@@ -0,0 +1,23 @@
+package com.rising.backend.domain.post.domain;
+
+import com.rising.backend.global.domain.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+}
