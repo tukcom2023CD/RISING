6e332f2723c6dfb656e0b22bb02e04140c637b97
suhyeon <suhyeon3484@naver.com>
2023-01-04 01:06:27 +0900

    chore: typescript, tailwind, prettier, eslint 재설정

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,9 +1,8 @@
 module.exports = {
-  singleQuote: true, // 문자열은 따옴표로 formatting
-  semi: false, //코드 마지막에 세미콜른이 없게 formatting
-  useTabs: false, //탭의 사용을 금하고 스페이스바 사용으로 대체하게 formatting
-  tabWidth: 2, // 들여쓰기 너비는 2칸
-  trailingComma: 'all', // 객체나 배열 키:값 뒤에 항상 콤마를 붙히도록 formatting
-  printWidth: 120, // 코드 한줄이 maximum 80칸
-  arrowParens: 'avoid', // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략하게 formatting
-}
+  tabWidth: 2, // 들여쓰기
+  semi: true, // 코드 마지막 줄  세미콜론
+  singleQuote: true, // string 사용시  ''
+  trailingComma: "all", // 객체나 배열의 마지막 워노 끝에도 , 사용
+  printWidth: 80, // 한 줄에 80자까지 보여주고 넘어가면 줄바꿈
+  arrowParens: "always", // 화살표 함수 매개변수 주위에 괄호 포함
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    content: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {},
+            colors: {},
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
