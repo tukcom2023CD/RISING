21dbb3389ba1507ba1f8608bab5163de5ac34c87
halin <halin130@naver.com>
2023-01-04 10:47:27 +0900

    feat: user create API 구현

--- backend/src/main/java/com/rising/backend/BackendApplication.java
+++ backend/src/main/java/com/rising/backend/BackendApplication.java
@@ -2,10 +2,11 @@ package com.rising.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/com/rising/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/rising/backend/global/config/SecurityConfig.java
@@ -2,16 +2,19 @@ package com.rising.backend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.httpBasic().disable();
     }
 }
