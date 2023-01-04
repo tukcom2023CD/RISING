a4f9c21c773c825d8dbf822c7bc12d077d0d80da
suhyeon <suhyeon3484@naver.com>
2023-01-04 12:02:11 +0900

    chore: eslint 수정

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   extends: [
     // 다른 config를 사용하더라도 prettier를 맨 마지막에 넣어야 모든 중복 규칙을 비활성화 시킬 수 있다.
     'airbnb',
-    'prettier/react',
+    'prettier',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:@typescript-eslint/recommended',
