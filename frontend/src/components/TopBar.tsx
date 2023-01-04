b97bae8268f8b8f53cf10ddcbc53b858b0854de8
suhyeon <suhyeon3484@naver.com>
2023-01-04 23:19:09 +0900

    feat: topbar

---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,38 @@
+import ColorSystem from 'utils/ColorSystem';
+import rogo from 'images/rogo.png';
+
+function TopBar() {
+  return (
+    <div
+      className="w-full h-30"
+      style={{ backgroundColor: ColorSystem.MainColor.Secondary }}
+    >
+      <div className="flex">
+        <img className="left-0 w-48 pl-12 pr-8 py-8" src={rogo} alt="rogo" />
+        <span className="text-xl  pl-10 pr-8 py-8 my-2">멘토구하기</span>
+        <span className="text-xl  pl-10 pr-8 py-8 my-2">멘토링 구경하기</span>
+        <span className=" text-xl  pl-10 pr-8 py-8 my-2">질문게시판</span>
+        <span className="text-xl  pl-10 pr-8 py-8 my-2">커뮤니티</span>
+        <div className="absolute top-0 right-0 mr-28">
+          <button
+            type="button"
+            className="rounded-3xl border-none h-10 w-20 mx-1 my-8"
+            style={{ backgroundColor: ColorSystem.MainColor.quartic }}
+          >
+            로그인
+          </button>
+          <button
+            type="button"
+            className="rounded-3xl border-none h-10 w-20 mx-1 my-8"
+            style={{ backgroundColor: ColorSystem.MainColor.tertiary }}
+          >
+            회원가입
+          </button>
+        </div>
+        <div className="leading-3" />
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
