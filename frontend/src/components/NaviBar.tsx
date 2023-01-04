0f0307ae33737b3bca28de3e0dd770ce3c98c953
suhyeon <suhyeon3484@naver.com>
2023-01-05 00:13:50 +0900

    feat: navibar 수정

--- frontend/src/components/NaviBar.tsx
+++ frontend/src/components/NaviBar.tsx
@@ -7,12 +7,20 @@ function TopBar() {
       className="w-full h-30 border-b-2"
       style={{ backgroundColor: ColorSystem.MainColor.Primary }}
     >
-      <div className="flex">
+      <div className="flex flex-row">
         <img className="left-0 w-48 pl-12 pr-8 py-8" src={rogo} alt="rogo" />
-        <span className="text-xl  pl-10 pr-8 py-8 my-2">멘토구하기</span>
-        <span className="text-xl  pl-10 pr-8 py-8 my-2">멘토링 구경하기</span>
-        <span className="text-xl  pl-10 pr-8 py-8 my-2">질문게시판</span>
-        <span className="text-xl  pl-10 pr-8 py-8 my-2">커뮤니티</span>
+        <button type="button" className="text-xl  pl-10 pr-8 py-8 my-2">
+          멘토구하기
+        </button>
+        <button type="button" className="text-xl  pl-10 pr-8 py-8 my-2">
+          멘토링 구경하기
+        </button>
+        <button type="button" className="text-xl  pl-10 pr-8 py-8 my-2">
+          질문게시판
+        </button>
+        <button type="button" className="text-xl  pl-10 pr-8 py-8 my-2">
+          커뮤니티
+        </button>
         <div className="absolute top-0 right-0 mr-28">
           <button
             type="button"
