6d42950b3ef3e90504c55007e32a895c1231a773
suhyeon <suhyeon3484@naver.com>
2023-01-04 23:24:49 +0900

    feat: Modify topbar

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -5,13 +5,13 @@ function TopBar() {
   return (
     <div
       className="w-full h-30"
-      style={{ backgroundColor: ColorSystem.MainColor.Secondary }}
+      style={{ backgroundColor: ColorSystem.MainColor.Primary }}
     >
       <div className="flex">
         <img className="left-0 w-48 pl-12 pr-8 py-8" src={rogo} alt="rogo" />
         <span className="text-xl  pl-10 pr-8 py-8 my-2">멘토구하기</span>
         <span className="text-xl  pl-10 pr-8 py-8 my-2">멘토링 구경하기</span>
-        <span className=" text-xl  pl-10 pr-8 py-8 my-2">질문게시판</span>
+        <span className="text-xl  pl-10 pr-8 py-8 my-2">질문게시판</span>
         <span className="text-xl  pl-10 pr-8 py-8 my-2">커뮤니티</span>
         <div className="absolute top-0 right-0 mr-28">
           <button
@@ -29,7 +29,6 @@ function TopBar() {
             회원가입
           </button>
         </div>
-        <div className="leading-3" />
       </div>
     </div>
   );
