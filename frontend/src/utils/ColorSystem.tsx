c53b803eff7af8f57f9b1e4ad821aba15eb1e382
suhyeon <suhyeon3484@naver.com>
2023-01-05 14:20:25 +0900

    chore: colorsystem

--- frontend/src/utils/ColorSystem.tsx
+++ frontend/src/utils/ColorSystem.tsx
@@ -1,11 +1,9 @@
-// 수정해야함
 const ColorSystem = {
   MainColor: {
-    Primary: '#FCFCFC',
+    Primary: '#F8F8FF',
     Secondary: '#E1E0FF',
     tertiary: '#9D9CD2',
     quartic: '#B0D79D',
-    text: '#54547D',
   },
 };
 
