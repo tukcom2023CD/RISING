b0724819f177fdb23900022279486b7d92124d2d
suhyeon <suhyeon3484@naver.com>
2023-01-06 01:35:36 +0900

    feat: 질문 리스트 페이지 상단바 구현

--- frontend/src/page/QuesListPage.tsx
+++ frontend/src/page/QuesListPage.tsx
@@ -1,11 +1,18 @@
 import 'tailwindcss/tailwind.css';
 import 'utils/pageStyle.css';
+import ColorSystem from 'utils/ColorSystem';
+import QuesNavBar from 'components/QuesNavBar';
 
 // 질문 리스트 페이지
 function QuesListPage() {
   return (
-    <div>
-      <p>QuesListPage</p>
+    // 배경색
+    <div
+      className="h-screen"
+      style={{ backgroundColor: ColorSystem.MainColor.Primary }}
+    >
+      {/* 상단바 */}
+      <QuesNavBar />
     </div>
   );
 }
