4c1077247d3f3dcf04183ffc96ac253c16b6910f
halin <halin130@naver.com>
2023-01-05 22:48:22 +0900

    feat: login, logout 기능 구현

--- backend/src/main/java/com/rising/backend/domain/post/dto/request/PostCreateRequestDto.java
+++ backend/src/main/java/com/rising/backend/domain/post/dto/request/PostCreateRequestDto.java
@@ -22,5 +22,4 @@ public class PostCreateRequestDto {
     @NotEmpty
     private PostType type;
 
-
 }
--- backend/src/main/java/com/rising/backend/domain/user/service/LoginService.java
+++ backend/src/main/java/com/rising/backend/domain/user/service/LoginService.java
@@ -2,6 +2,7 @@ package com.rising.backend.domain.user.service;
 
 import com.rising.backend.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
@@ -9,24 +10,25 @@ import javax.servlet.http.HttpSession;
 @Service
 @RequiredArgsConstructor
 public class LoginService {
-    private final HttpSession httpSession;
     private final UserService userService;
+
+    private final PasswordEncoder passwordEncoder;
 
     public static final String USER_ID = "USER_ID";
 
-    public void login(Long id) {
-        httpSession.setAttribute(USER_ID, id);
+    public void login(Long id, HttpSession session) {
+        session.setAttribute(USER_ID, id);
     }
 
-    public void logout() {
-        httpSession.removeAttribute(USER_ID);
+    public void logout(HttpSession session) {
+        session.removeAttribute(USER_ID);
+        session.invalidate();
     }
 
     public boolean checkPassword(String username, String checkPassword) {
         User member = userService.findUserByUsername(username);
         String realPassword = member.getPassword();
-//        boolean matches = encoder.matches(checkPassword, realPassword);
-//        return matches;
+        return passwordEncoder.matches(checkPassword, realPassword);
     }
 
     public User getLoginUser() {
