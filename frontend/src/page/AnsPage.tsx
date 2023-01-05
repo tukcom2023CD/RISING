bcaf1bdeb4fb8d1e39131732958290e9eba5f918
suhyeon <suhyeon3484@naver.com>
2023-01-05 14:20:38 +0900

    feat: anspage

--- frontend/src/page/AnsPage.tsx
+++ frontend/src/page/AnsPage.tsx
@@ -1,11 +1,35 @@
 import 'tailwindcss/tailwind.css';
 import 'utils/pageStyle.css';
 
+import ColorSystem from 'utils/ColorSystem';
+import NavBar from 'components/NavBar';
+import Tag from 'components/Tag';
+
+interface Props {
+  text: Text;
+}
+
 // 질문 답변 및 확인 페이지
 function AnsPage() {
   return (
-    <div>
-      <p>AnsPage</p>
+    <div
+      className="h-screen"
+      style={{ backgroundColor: ColorSystem.MainColor.Primary }}
+    >
+      <NavBar />
+      {/* Title */}
+      <div className="grid place-items-center">
+        <p className="text-text-color text-2xl">Title</p>
+        <div className="flex flex-col rounded-xl h-32 w-4/5 mx-1 my-2 border-4 border-violet-300">
+          {/* 질문 제목 텍스트로 가져와야함 */}
+          <span className="text-xl m-4">질문제목</span>
+          <div className="flex flex-row">
+            <Tag text="#JavaScript" />
+            <Tag text="#python" />
+          </div>
+        </div>
+      </div>
+      {/* Title 끝 */}
     </div>
   );
 }
