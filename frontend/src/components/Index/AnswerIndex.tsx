888c3f886afac182819237a8577ea43aaa20d3bd
suhyeon <suhyeon3484@naver.com>
2023-01-05 22:02:40 +0900

    refactor: components 이름 변경

--- frontend/src/components/Index/AnswerIndex.tsx
+++ frontend/src/components/Index/AnswerIndex.tsx
@@ -1,23 +1,23 @@
-import answer1 from 'images/answer1.png';
-import answer2 from 'images/answer2.png';
-import answer3 from 'images/answer3.png';
+import end1 from 'images/end1.png';
+import end2 from 'images/end2.png';
+import end3 from 'images/end3.png';
 
 function TitleIndex() {
   return (
     <div>
       <img
         className="absolute bottom-14 right-20 py-2 w-6"
-        src={answer1}
+        src={end1}
         alt="index1"
       />
       <img
         className="absolute bottom-14 right-12 py-2 w-6"
-        src={answer2}
+        src={end2}
         alt="index1"
       />
       <img
         className="absolute bottom-14 right-4 py-1 w-6"
-        src={answer3}
+        src={end3}
         alt="index1"
       />
     </div>
