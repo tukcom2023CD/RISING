99b595acf714d715d4a91b624fc2cf9f7ed7c8f8
suhyeon <suhyeon3484@naver.com>
2023-01-06 11:14:50 +0900

    refactor: box size

--- frontend/src/page/PrivateAnsPage.tsx
+++ frontend/src/page/PrivateAnsPage.tsx
@@ -26,10 +26,10 @@ function PrivateAnsPage() {
       {/* Title */}
       <div className="flex justify-center item-center my-8">
         <div className="relative flex flex-col-reverse w-3/5">
-          <div className="flex flex-col rounded-xl h-32 w-full mx-1 my-2 bg-white border-4 border-violet-300">
+          <div className="flex flex-col rounded-xl h-28 w-full mx-1 my-2 bg-white border-4 border-violet-300">
             {/* 질문 제목 텍스트로 가져와야함 */}
-            <span className="text-text-color text-xl m-4">질문 제목</span>
-            <div className="flex flex-row relative">
+            <span className="text-text-color text-xl mt-4 mx-4">질문 제목</span>
+            <div className="my-2 flex flex-row relative">
               <Tag text="# JavaScript" />
               <Tag text="# python" />
               <div className="absolute top-0 right-3">
