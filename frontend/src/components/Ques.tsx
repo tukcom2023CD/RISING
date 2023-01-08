0ddd56e30b68b59c938f9d020aacfaf2c86e6827
suhyeon <suhyeon3484@naver.com>
2023-01-08 22:25:52 +0900

    chore

--- frontend/src/components/Ques.tsx
+++ frontend/src/components/Ques.tsx
@@ -10,7 +10,7 @@ function Ques({ count, title, date }: Props) {
   return (
     <div className="relative flex flex-row h-22 items-center w-full bg-white border-2 border-gray rounded-lg">
       {/* 답변 수 */}
-      <div className="h-14 w-14 my-4 ml-4 mr-2 rounded-lg bg-violet-100 hover:bg-violet-300">
+      <div className="h-14 w-14 my-4 ml-4 mr-2 rounded-lg bg-violet-100">
         <div className="flex justify-center item-center">
           <div className="flex-col m-1">
             <p className="m-0.5 mx-2 px-0.5 text-sm">{count}</p>
