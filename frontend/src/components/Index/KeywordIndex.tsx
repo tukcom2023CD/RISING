dbbdb6964584710c736389fc35f140b132ab1f2d
suhyeon <suhyeon3484@naver.com>
2023-01-08 20:17:54 +0900

    feat: keyword, content

---
+++ frontend/src/components/Index/KeywordIndex.tsx
@@ -0,0 +1,27 @@
+import middle1 from 'images/middle1.png';
+import middle2 from 'images/middle2.png';
+import middle3 from 'images/middle3.png';
+
+function KeywordIndex() {
+  return (
+    <div>
+      <img
+        className="absolute bottom-14 right-20 py-2 w-6"
+        src={middle1}
+        alt="index1"
+      />
+      <img
+        className="absolute bottom-14 right-12 py-1 w-6"
+        src={middle2}
+        alt="index1"
+      />
+      <img
+        className="absolute bottom-14 right-4 py-2 w-6"
+        src={middle3}
+        alt="index1"
+      />
+    </div>
+  );
+}
+
+export default KeywordIndex;
--- frontend/src/page/QuesPage.tsx
+++ frontend/src/page/QuesPage.tsx
@@ -1,8 +1,11 @@
 import 'tailwindcss/tailwind.css';
 import 'utils/pageStyle.css';
 import ColorSystem from 'utils/ColorSystem';
-import TitleIndex from 'components/Index/QuesTitleIndex';
 import MentoNavBar from 'components/MentoNavBar';
+import KeywordSelect from 'components/Select/KeywordSelect';
+import TitleIndex from 'components/Index/QuesTitleIndex';
+import ContentIndex from 'components/Index/MemoIndex';
+import KeywordIndex from 'components/Index/KeywordIndex';
 
 // 질문 작성 페이지
 function QuesPage() {
@@ -24,6 +27,38 @@ function QuesPage() {
           {/* title index */}
           <TitleIndex />
           <span className="pl-3 text-text-color text-2xl">TITLE</span>
+        </div>
+      </div>
+      {/* Keyword */}
+      <div className="flex justify-center item-center my-8">
+        <div className="relative flex flex-col-reverse w-3/5">
+          <div className="flex flex-col rounded-xl h-14 w-full mx-1 my-2 bg-white border-4 border-violet-300">
+            {/* 키워드 작성 */}
+            <div className="pt-1 px-1">
+              <KeywordSelect />
+            </div>
+          </div>
+          {/* keyword index */}
+          <KeywordIndex />
+          <span className="pl-3 text-text-color text-2xl">KEYWORD</span>
+        </div>
+      </div>
+      {/* Content */}
+      <div className="flex justify-center item-center my-8">
+        <div className="relative flex flex-col-reverse w-3/5">
+          <div className="flex flex-col rounded-xl h-64 w-full pr-4 mx-1 my-2 bg-white border-4 border-violet-300">
+            {/* 질문할 내용 작성 */}
+            <div className="relative">
+              <input
+                type="text"
+                className="absolute top-1 left-2 w-full h-10 rounded-lg focus:shadow focus:outline-none"
+                placeholder="content.."
+              />
+            </div>
+          </div>
+          {/* Record video index */}
+          <ContentIndex />
+          <span className="pl-3 text-text-color text-2xl">CONTENT</span>
         </div>
       </div>
     </div>
