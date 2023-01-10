4fe21377783b55fb73868fa981fb24c35df2c657
jhkim <wellsy001012@gmail.com>
2023-01-10 10:31:14 +0900

    feat: rabbitmq postId로 메세지 구분

--- backend/src/main/java/com/rising/backend/domain/sharecoding/controller/CodeController.java
+++ backend/src/main/java/com/rising/backend/domain/sharecoding/controller/CodeController.java
@@ -5,10 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import static com.rising.backend.global.constant.RabbitMQ.CODE_EXCHANGE_NAME;
@@ -20,15 +20,15 @@ import static com.rising.backend.global.constant.RabbitMQ.CODE_QUEUE_NAME;
 public class CodeController {
     private final RabbitTemplate rabbitTemplate;
 
-    @MessageMapping("/code/message")
-    @PostMapping("/code/message")
-    @SendTo("/topic/code")
-    public Operation send(@RequestBody Operation operation) {
-        rabbitTemplate.convertAndSend(CODE_EXCHANGE_NAME, "code.share", operation);
+    @MessageMapping("/code/message/{postId}") //웹소켓으로 들어오는 메세지 발행 요청 처리. 클라이언트는 (/pub)/code/message/10 이런식으로 전송
+//    @PostMapping("/code/message{postId}")
+    @SendTo("/topic/{postId}")
+    public Operation send(@DestinationVariable Long postId, @RequestBody Operation operation) {
+        rabbitTemplate.convertAndSend(CODE_EXCHANGE_NAME, "code.share", operation); //클라이언트로 전송
         return operation;
     }
 
-    @RabbitListener(queues = CODE_QUEUE_NAME)
+    @RabbitListener(queues = CODE_QUEUE_NAME) //해당 큐에 들어온 메세지 소비
     public void receive(Operation operation) {
         log.info("operation.getText = {}", operation.getText());
     }
--- backend/src/main/java/com/rising/backend/global/config/StompConfig.java
+++ backend/src/main/java/com/rising/backend/global/config/StompConfig.java
@@ -13,12 +13,12 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 //        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*").withSockJS();
-        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*"); //웹소켓 엔드포인트 지정. 클라이언트에서 이 경로로 서버와 handshake / 허용할 origin패턴 지정
     }
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.setApplicationDestinationPrefixes("/pub");
-        config.enableSimpleBroker("/sub");
+    public void configureMessageBroker(MessageBrokerRegistry config) { // 메세지브로커 활성화
+        config.setApplicationDestinationPrefixes("/pub"); //클라이언트에서 발송한 메세지 prefix 자동으로 붙음 / s컨트롤러에서는 이 prefix를 제외한 경로로 mapping하면 됨
+        config.enableSimpleBroker("/sub"); //메세지브로커 활성화 / subscribe접두사 설정 - 경로: sub/messagemapping설정경로
     }
 }
 
