427e751cc26d8180d9754974784575726043aeca
KoneJ <kbvv67890@gmail.com>
2023-01-11 23:58:06 -0800

    feat: 로그인 회원가입 페이지 퍼블리싱 및 프로필사진 업로드 1차 구현

--- frontend/src/page/LoginPage.tsx
+++ frontend/src/page/LoginPage.tsx
@@ -2,8 +2,14 @@ import 'tailwindcss/tailwind.css';
 import 'utils/pageStyle.css';
 import ColorSystem from 'utils/ColorSystem';
 import NavBar from 'components/NavBar';
+import Button from 'components/LoginBtn';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/mainpage');
+  };
   return (
     <div
       className="h-screen"
@@ -12,10 +18,16 @@ function LoginPage() {
       {/* 상단바 */}
       <NavBar />
       {/* 로그인 틀 */}
-      <div className="flex justify-center item-center my-8 mt- h-2/5">
-        <div className="w-3/5 bg-white drop-shadow-lg item-center pr-2">
-          <div className="flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
-            {/*  */}
+      <div className=" mt-36 flex justify-center item-center my-8 mt- h-2/5">
+        <div className="w-2/5 bg-white drop-shadow-lg item-center pr-2">
+          <div className="mt-16 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
+            {}
+          </div>
+          <div className="mt-8 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
+            {}
+          </div>
+          <div className='mt-6 grid place-items-center'>
+            <Button text="Login" onClick={goToMain}/>
           </div>
         </div>
       </div>
--- frontend/src/page/SignUpPage.tsx
+++ frontend/src/page/SignUpPage.tsx
@@ -1,10 +1,48 @@
 import 'tailwindcss/tailwind.css';
 import 'utils/pageStyle.css';
+import ColorSystem from 'utils/ColorSystem';
+import NavBar from 'components/NavBar';
+import Button from 'components/LoginBtn';
+import { useNavigate } from 'react-router-dom';
+import ProfileUpload from '../components/ProfileUpload.js';
 
 function SignUpPage() {
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate('/login');
+  };
   return (
-    <div>
-      <p>SignUpPage</p>
+<div
+      className="h-screen"
+      style={{ backgroundColor: ColorSystem.MainColor.Primary }}
+    >
+      {/* 상단바 */}
+      <NavBar />
+      {/* 회원가입 틀 */}
+      <div className=" mt-36 flex justify-center item-center my-8 mt- h-2/5">
+        <div className="w-2/5 bg-white drop-shadow-lg item-center pr-2">
+          <div className="">
+            <ProfileUpload/>
+          </div>
+            <div>
+              <div className="mt-16 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
+                {}
+              </div>
+              <div className="mt-8 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
+                {}
+              </div>
+              <div className="mt-8 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
+                {}
+              </div>
+              <div className="mt-8 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
+                {}
+              </div>
+            </div>
+          <div className='mt-6 grid place-items-center'>
+            <Button text="다음" onClick={goToLogin}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
