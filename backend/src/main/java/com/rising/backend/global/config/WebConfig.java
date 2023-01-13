3120873b4ca0cfc16719422d0d1cbd3e2b7e6fef
halin <halin130@naver.com>
2023-01-13 22:34:06 +0900

    feat: cors 설정

--- backend/src/main/java/com/rising/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/rising/backend/global/config/WebConfig.java
@@ -2,10 +2,10 @@ package com.rising.backend.global.config;
 
 import com.rising.backend.global.interceptor.LoginInterceptor;
 import com.rising.backend.global.resolver.LoginUserArgumentsResolver;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
     private final LoginInterceptor loginInterceptor;
     private final LoginUserArgumentsResolver loginUserArgumentsResolver;
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+    
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentsResolver);
