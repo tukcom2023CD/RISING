8f8bbb219f57f3de6d89129fcdd4116c80b7d1ba
suhyeon <suhyeon3484@naver.com>
2023-01-16 01:28:29 +0900

    feat: 중복 삭제

--- frontend/src/components/Select/KeywordData.ts
+++ frontend/src/components/Select/KeywordData.ts
@@ -9,7 +9,6 @@ export const keywordOptions: readonly keywordOption[] = [
   { value: 'Python', color: '#835FEE', label: 'Python' },
   { value: 'JavaScript', color: '#835FEE', label: 'JavaScript' },
   { value: 'TypeScript', color: '#835FEE', label: 'TypeScript' },
-  { value: 'JavaScript', color: '#835FEE', label: 'JavaScript' },
   { value: 'Spring', color: '#835FEE', label: 'Spring' },
   { value: 'React', color: '#835FEE', label: 'React' },
 ];
