2e0a45a5661ecdb1b4694875c83bb33f864257bc
jhkim <wellsy001012@gmail.com>
2023-01-15 21:29:35 +0900

    feat: fe 로그인 코드

--- frontend/src/page/LoginPage.tsx
+++ frontend/src/page/LoginPage.tsx
@@ -3,13 +3,46 @@ import 'utils/pageStyle.css';
 import ColorSystem from 'utils/ColorSystem';
 import NavBar from 'components/NavBar';
 import Button from 'components/LoginBtn';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 function LoginPage() {
-  const navigate = useNavigate();
-  const goToMain = () => {
-    navigate('/mainpage');
-  };
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  // const navigate = useNavigate();
+  // const goToMain = () => {
+  //   navigate('/mainpage');
+  // };
+  const checkUser = () => {
+    if (username === "" || password === "") {
+      // eslint-disable-next-line no-alert
+      alert("아이디와 비밀번호를 입력해주세요!");
+    }
+    // else{
+    //   navigate("/afterlogin");
+    // }
+  }
+axios
+.post('http://127.0.0.1:8080/api/v1/users/login', {
+  username,
+  password,
+})
+.then(response => {
+  // Handle success.
+  console.log('로그인 완료!');
+  console.log('유저 네임:', username);
+  console.log('유저 세션', response.data.session);
+  console.log('유저 프로필', response.data.user);
+  console.log('유저 아이디', response.data.user.id);
+  sessionStorage.setItem('username',username);
+  sessionStorage.setItem('session' , response.data.session.access);
+  sessionStorage.setItem('user_id',response.data.user.id);
+})
+.catch(error => {
+  // Handle error.
+  console.log('에러 발생!', error.response);
+});
   return (
     <div
       className="h-screen"
@@ -24,21 +57,32 @@ function LoginPage() {
             <input
               className="h-9 m-1 placeholder-[#9CA6C5]"
               placeholder="Email Address"
+              type='email'
+              value={username}
+              required
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
             />
           </div>
           <div className="mt-8 flex flex-col rounded-xl h-14 w-full mx-1 my-3 bg-white border-4 border-violet-300">
             <input
               className="h-9 m-1 placeholder-[#9CA6C5]"
               placeholder="Password"
+              type='password'
+              value={password}
+              required
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
             />
           </div>
           <div className="mt-6 grid place-items-center">
-            <Button text="Login" onClick={goToMain} />
+            <Button text="Login" onClick={checkUser}/>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default LoginPage;
