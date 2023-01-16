970ff725d62e67a0ee6c0ee0b6f82dd3a9239304
suhyeon <suhyeon3484@naver.com>
2023-01-16 13:28:35 +0900

    feat: 질문 리스트 컴포넌트 props 추가에 따른 주석처리

--- frontend/src/page/MyPage.tsx
+++ frontend/src/page/MyPage.tsx
@@ -88,12 +88,13 @@ function MyPage() {
                   >
                     <div className="h-64">
                       <div className="flex flex-col p-1">
-                        <Ques count={2} title="질문 제목1" date="2023-01-04" />
+                        {/* 내가 작성한 질문 리스트 */}
+                        {/* <Ques count={2} title="질문 제목1" date="2023-01-04" />
                         <Ques count={1} title="질문 제목2" date="2023-01-05" />
                         <Ques count={4} title="질문 제목3" date="2023-01-06" />
                         <Ques count={3} title="질문 제목4" date="2023-01-07" />
                         <Ques count={6} title="질문 제목5" date="2023-01-08" />
-                        <Ques count={2} title="질문 제목6" date="2023-01-08" />
+                        <Ques count={2} title="질문 제목6" date="2023-01-08" /> */}
                       </div>
                     </div>
                   </div>
