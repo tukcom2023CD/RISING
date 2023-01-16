1266ac4afa67156a82d3081195dd47e6ac3107bc
suhyeon <suhyeon3484@naver.com>
2023-01-17 02:32:20 +0900

    chore: 에러 수정

--- frontend/src/components/Editor/EditorViewer.tsx
+++ frontend/src/components/Editor/EditorViewer.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Viewer } from '@toast-ui/react-editor';
-import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 
 interface Props {
