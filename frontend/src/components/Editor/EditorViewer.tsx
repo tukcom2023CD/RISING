ad98cc2c8347bb0ad2d781d2b70166d8fcd45fab
suhyeon <suhyeon3484@naver.com>
2023-01-17 00:30:08 +0900

    feat: viewer editor, 필요없는 주석 제거

---
+++ frontend/src/components/Editor/EditorViewer.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Viewer } from '@toast-ui/react-editor';
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+
+interface Props {
+  content: string;
+}
+
+function EditorViewer({ content = '' }: Props) {
+  return <>{content && <Viewer initialValue={content || ''} />}</>;
+}
+
+export default EditorViewer;
