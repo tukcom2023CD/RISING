d0bc1e63ab5a628f54bac77c75a53742b17218a2
KoneJ <kbvv67890@gmail.com>
2023-01-17 20:55:35 -0800

    feat: 일반 질문 답변 페이지 api 불러오기 구현

--- frontend/src/page/AnsPage.tsx
+++ frontend/src/page/AnsPage.tsx
@@ -9,9 +9,43 @@ import ContentIndex from 'components/Index/ContentIndex';
 import AnswerIndex from 'components/Index/AnswerIndex';
 import Ans from 'components/Ans/Ans';
 import ReAns from 'components/Ans/ReAns';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import EditorViewer from 'components/Editor/EditorViewer';
 
 // 질문 답변 및 확인 페이지
 function AnsPage() {
+  const location = useLocation();
+  const state = location.state as { id: number };
+  const postId = state.id;
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [userId, setUserId] = useState(0);
+  const [tags, setTags] = useState([]);
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      await axios
+        // 특정 게시글 조회
+        // 질문 게시글에서 질문 아이디 받아와야함.
+        .get(`/posts/${postId}`)
+        .then((res) => {
+          console.log(res.data.data);
+          setTitle(res.data.data.title);
+          setContent(res.data.data.content);
+          setUserId(res.data.data.userId);
+          setTags(res.data.data.tags);
+          setDate(res.data.data.created_at);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })();
+  }, []);
+
   return (
     // 배경색
     <div
@@ -25,12 +59,13 @@ function AnsPage() {
         <div className="relative flex flex-col-reverse w-3/5">
           <div className="flex flex-col rounded-xl h-28 w-full mx-1 my-2 bg-white border-4 border-violet-300">
             {/* 질문 제목 텍스트로 가져와야함 */}
-            <span className="text-text-color text-xl mt-4 mx-4">질문 제목</span>
+            <span className="text-text-color text-xl mt-4 mx-4">{title}</span>
             <div className="my-2 pl-2 flex flex-row relative">
-              <Tag text="# JavaScript" />
-              <Tag text="# python" />
+              {tags.map((tag: any) => (
+                <Tag key={Math.random() * 500} text={tag} />
+              ))}
               <div className="absolute top-0 right-0">
-                <Date date="2023-01-04" />
+                <Date date={date} />
               </div>
             </div>
           </div>
@@ -42,14 +77,16 @@ function AnsPage() {
       {/* Content */}
       <div className="flex justify-center item-center my-8">
         <div className="relative flex flex-col-reverse w-3/5">
-          <div className="flex flex-col rounded-xl h-[20rem] w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300">
             {/* 코드 에디터 */}
             <div className="flex justify-center item-center mb-8">
               <div className="relative flex flex-col-reverse w-full">
-                {/* <ToastEditor editorRef={ref}/> */}
+                <div className="flex flex-col rounded-xl h-[20rem] w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300">
+                  <div className="pl-3 pt-2">
+                    <EditorViewer content={content} />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
           {/* content index */}
           <ContentIndex />
           <span className="pl-3 text-text-color text-2xl">CONTENT</span>
@@ -98,3 +135,5 @@ function AnsPage() {
 }
 
 export default AnsPage;
+
+
