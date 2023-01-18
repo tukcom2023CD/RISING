f347fa16d83cc293a962fe74c7e2d60a8162a9d1
jhkim <wellsy001012@gmail.com>
2023-01-18 18:34:03 +0900

    feat: 현재 user가 mentee인 채팅방 조회 api

--- backend/src/main/java/com/rising/backend/domain/chat/repository/ChatRoomRepository.java
+++ backend/src/main/java/com/rising/backend/domain/chat/repository/ChatRoomRepository.java
@@ -3,5 +3,8 @@ package com.rising.backend.domain.chat.repository;
 import com.rising.backend.domain.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    List<ChatRoom> findByMentee_Id(Long id);
 }
