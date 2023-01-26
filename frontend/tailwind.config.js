2943268342c082a2a51903386e05801d6adcd1ac
suhyeon <suhyeon3484@naver.com>
2023-01-26 16:34:44 +0900

    chore: 필요없는 코드 제거

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -22,16 +22,6 @@ module.exports = {
           '0%, 100%': { transform: 'translate(0, 0)' },
           '50%': { transform: 'translate(-50px, 50px)' },
         },
-        'custom-bounce': {
-          '0%, 100%': {
-            transform: 'translateY(-25%)',
-            // 'animation-timing-function': cubic - bezier(0.8, 0, 1, 1),
-          },
-          '50%': {
-            transform: 'translateY(0)',
-            // 'animation-timing-function': cubic - bezier(0, 0, 0.2, 1),
-          },
-        },
         typingCursor: {
           from: {
             borderRight: '2px solid while',
@@ -45,7 +35,6 @@ module.exports = {
         wiggle: 'wiggle 1s ease-in-out infinite',
         'move-right': 'move-right 3s linear forwards infinite',
         'move-left': 'move-left 3s linear forwards infinite',
-        'custom-bounce': 'bounce 1s infinite',
         typingCursor: 'typingCursor 1s ease-in-out 0ms infinite',
       },
     },
