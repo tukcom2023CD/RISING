34d681508a20a9f93780e4eb6e3a68fe835ec691
KoneJ <kbvv67890@gmail.com>
2023-03-12 05:03:38 +0900

    feat: 일반 질문 답변 인풋 박스 반응형 구현

--- frontend/src/components/Ans/Ans.tsx
+++ frontend/src/components/Ans/Ans.tsx
@@ -7,16 +7,16 @@ interface Props {
 
 function Ans({ ans, person, date, time }: Props) {
   return (
-    <div className="relative flex flex-row h-14 items-center m-0.5 w-full border-2 border-gray rounded-lg">
+      <div className="relative flex flex-row items-center p-2 mb-2 rounded-lg border-2 border-gray">
       {/* 프로필 사진 */}
-      <div className="h-9 w-9 m-2 rounded-lg bg-violet-100" />
+      <div className="h-9 w-9 mr-2 rounded-full bg-violet-100" />
       {/* 답변자와 답변 */}
-      <div className="flex flex-col">
-        <span className="font-bold text-xs">{person}</span>
+      <div className="flex flex-col flex-grow">
+        <span className="font-bold text-xs ">{person}</span>
         <span className="text-xs">{ans}</span>
       </div>
       {/* 날짜 */}
-      <div className="absolute top-3 right-3 text-xs">
+      <div className="ml-2 text-xs whitespace-nowrap">
         {date} {time}
       </div>
     </div>
--- frontend/src/page/AnsPage.tsx
+++ frontend/src/page/AnsPage.tsx
@@ -143,8 +143,8 @@ function AnsPage() {
           {/* 코드 에디터 */}
           <div className="flex justify-center item-center mb-8">
             <div className="relative flex flex-col-reverse w-full">
-              <div className="flex flex-col rounded-xl h-[20rem] w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300">
-                <div className="pl-3 pt-2">
+              <div className="flex flex-col rounded-xl h-full w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300">
+                <div className="pl-3 ">
                   <EditorViewer content={content} />
                 </div>
               </div>
