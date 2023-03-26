ae7cf42ea0238411bbbfd36e0688fec06575af80
halin <halin130@naver.com>
2023-03-27 00:27:03 +0900

    fix: Spring boot Dockerfile 수정

--- backend/src/main/java/com/rising/backend/global/config/StompConfig.java
+++ backend/src/main/java/com/rising/backend/global/config/StompConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class StompConfig implements WebSocketMessageBrokerConfigurer{
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
