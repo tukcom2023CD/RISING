dc20d6a9765e9a945ca7c6ac775cb0e79866d6de
jhkim <wellsy001012@gmail.com>
2023-03-29 20:40:41 +0900

    chore: 스크립트 도메인 변경

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(rising-aha.net) // 각자 등록한 도메인
+domains=rising-aha.net // 각자 등록한 도메인
 rsa_key_size=4096
 data_path="./data/certbot"
 email="wellsy001012@gamil.com" # Adding a valid address is strongly recommended
