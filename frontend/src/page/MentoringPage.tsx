a360cddbab4b2973e93e45db5927063b8a81b574
KoneJ <kbvv67890@gmail.com>
2023-04-02 02:43:10 +0900

    feat: 멘토링 페이지 코드에디터 반응형 구현

--- frontend/src/page/MentoringPage.tsx
+++ frontend/src/page/MentoringPage.tsx
@@ -147,23 +147,23 @@ function MentoringPage() {
         <div className="relative flex flex-col-reverse w-3/5">
           {/* 코드 에디터 */}
           <div className="flex justify-center item-center mb-8">
-            <div className="relative flex flex-col-reverse w-full">
-              <div className="rounded-xl h-[20rem] w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300">
-                <CodeEditor
-                  value={codeList}
-                  language="py"
-                  placeholder="Please enter Python code."
-                  onChange={handleEditorChange}
-                  padding={15}
-                  style={{
-                    fontFamily:
-                      'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-                    fontSize: 12,
-                  }}
-                />
-              </div>
-            </div>
-          </div>
+  <div className="relative flex flex-col-reverse w-full">
+    <div className="rounded-xl h-[20rem] w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300 overflow-y-auto">
+      <CodeEditor
+        value={codeList}
+        language="py"
+        placeholder="Please enter Python code."
+        onChange={handleEditorChange}
+        padding={15}
+        style={{
+          fontFamily:
+            'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+          fontSize: 12,
+        }}
+      />
+    </div>
+  </div>
+</div>
           <ContentIndex />
           <span className="pl-3 text-text-color text-2xl">CONTENT</span>
         </div>
