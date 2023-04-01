850cab203e01d255418590d55292f0abfc63b394
KoneJ <kbvv67890@gmail.com>
2023-04-02 02:30:13 +0900

    feat: 코드 에디터 충돌 방지 오류 해결

--- frontend/src/page/MentoringPage.tsx
+++ frontend/src/page/MentoringPage.tsx
@@ -12,9 +12,7 @@ import CodeEditor from '@uiw/react-textarea-code-editor';
 import React, { useEffect, useRef, useState } from 'react';
 import { Client, IMessage } from '@stomp/stompjs';
 import axios from 'axios';
-// import voice from 'images/voice.png';
-// import screen from 'images/screen.png';
-// import record from 'images/record.png';
+
 
 function MentoringPage() {
   document.documentElement.setAttribute('data-color-mode', 'light');
@@ -48,27 +46,30 @@ function MentoringPage() {
     })();
   }, []);
 
-  // code editor
   const [codeList, setCodeList] = React.useState(`print(hello world)`);
-  const textRef = React.useRef(null);
-
-  /** 코드 데이터를 destination에 publish(이벤트 발행, 전송) */
-  useEffect(() => {
-    if (!client.current?.connected) return;
-    client.current.publish({
-      destination: `/pub/code.message.${postId}`,
-      body: JSON.stringify({
-        text: `${codeList}`,
-      }),
-    });
-  }, [codeList]);
-
-  // 바뀐코드 보내기
+  // const textRef = React.useRef<HTMLInputElement>(null);
+  
   const handleSub = (body: IMessage) => {
     const jsonBody = JSON.parse(body.body);
     console.log(jsonBody.text);
     setCodeList(jsonBody.text);
   };
+  
+  // 코드 에디터의 onChange 이벤트에서 웹소켓을 통해 코드를 전송
+  const handleEditorChange = (evn: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setCodeList(evn.target.value);
+  
+    if (!client.current?.connected) return;
+    client.current.publish({
+      destination: `/pub/code.message.${postId}`,
+      body: JSON.stringify({
+        text: `${evn.target.value}`,
+      }),
+    });
+  };
+
 
   const client = useRef<Client>();
 
@@ -76,7 +77,7 @@ function MentoringPage() {
   const connect = () => {
     client.current = new Client({
       // http 일경우 ws를 https일 경우 wss
-      brokerURL: 'ws://www.rising-aha.net/stomp',
+      brokerURL: 'ws://localhost:8080/stomp',
       reconnectDelay: 200000,
       heartbeatIncoming: 16000,
       heartbeatOutgoing: 16000,
@@ -130,7 +131,7 @@ function MentoringPage() {
               {title}
             </span>
             <div className="my-2 pl-2 flex flex-row relative">
-              {tags.map((tag: any) => (
+              {tags.map((tag: string) => (
                 <Tag key={Math.random() * 500} text={tag} />
               ))}
               <div className="absolute top-0 right-1">
@@ -142,21 +143,6 @@ function MentoringPage() {
           <span className="pl-3 text-text-color text-2xl">TITLE</span>
         </div>
       </div>
-      {/* 음성 채팅, 화면 공유, 기록 -> 추가 기능 */}
-      {/* <div className="flex justify-center item-center my-8"> */}
-      {/* <div>
-          <button type="button">
-            <img className="w-13 h-10" src={voice} alt="Record" />
-          </button>
-          <button type="button">
-            <img className="w-13 h-10" src={screen} alt="Record" />
-          </button>
-          <button type="button">
-            <img className="w-13 h-10" src={record} alt="Record" />
-          </button>
-        </div> */}
-      {/* </div> */}
-      {/* Content */}
       <div className="flex justify-center item-center my-8">
         <div className="relative flex flex-col-reverse w-3/5">
           {/* 코드 에디터 */}
@@ -165,12 +151,9 @@ function MentoringPage() {
               <div className="rounded-xl h-[20rem] w-full mx-1 my-2 pt-1.5 px-1 bg-white border-4 border-violet-300">
                 <CodeEditor
                   value={codeList}
-                  ref={textRef}
                   language="py"
                   placeholder="Please enter Python code."
-                  onChange={(evn: {
-                    target: { value: React.SetStateAction<string> };
-                  }) => setCodeList(evn.target.value)}
+                  onChange={handleEditorChange}
                   padding={15}
                   style={{
                     fontFamily:
@@ -193,3 +176,4 @@ function MentoringPage() {
 }
 
 export default MentoringPage;
+
