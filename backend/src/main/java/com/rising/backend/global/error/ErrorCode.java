fec2340354f4680dbec0eab7f6f9daa07124b240
halin <halin130@naver.com>
2023-04-06 19:00:25 +0900

    hotfix: Errorcode 인자 타입 수정

--- backend/src/main/java/com/rising/backend/global/error/ErrorCode.java
+++ backend/src/main/java/com/rising/backend/global/error/ErrorCode.java
@@ -13,15 +13,15 @@ public enum ErrorCode {
   INPUT_INVALID_VALUE(HttpStatus.BAD_REQUEST, "G002", "잘못된 입력"),
 
   // User 도메인
-  EXAMPLE_USER_ERROR(400, "U001", "테스트용 예시 에러코드"),
-  USER_LOGIN_REQURED(401,"U002","로그인 필요"),
-  USER_NOT_FOUND(404, "U003","해당 유저를 찾을 수 없음"),
-  USER_INVALID_PASSWORD(401, "U004","로그인 정보가 일치하지 않음"),
-  USER_NAME_DUPLICATED(409, "U005", "이미 해당 username을 가진 유저가 존재함" ),
+  EXAMPLE_USER_ERROR(HttpStatus.BAD_REQUEST, "U001", "테스트용 예시 에러코드"),
+  USER_LOGIN_REQURED(HttpStatus.UNAUTHORIZED,"U002","로그인 필요"),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U003","해당 유저를 찾을 수 없음"),
+  USER_INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "U004","로그인 정보가 일치하지 않음"),
+  USER_NAME_DUPLICATED(HttpStatus.CONFLICT, "U005", "이미 해당 username을 가진 유저가 존재함" ),
 
   //ChatRoom, ChatMessage 도메인
-  CHATROOM_NOT_FOUND(404, "CR001", "채팅방이 존재하지 않음"),
-  CHATROOM_CREATION_FAILED(409, "CR002", "자신의 글에서 채팅방 생성 불가능"),
+  CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "CR001", "채팅방이 존재하지 않음"),
+  CHATROOM_CREATION_FAILED(HttpStatus.CONFLICT, "CR002", "자신의 글에서 채팅방 생성 불가능"),
 
   // 도메인 - Post
   POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "해당 게시글 찾을 수 없음"),
